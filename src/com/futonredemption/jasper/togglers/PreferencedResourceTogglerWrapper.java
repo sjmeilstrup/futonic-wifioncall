4a966dd5e02fd11888ce1133d2a60ff51a9f7222
futonredemption <futonredemption@localhost>
2011-12-31 17:21:30 -0500

    Preferenced backed togglers.

--- src/com/futonredemption/jasper/togglers/PreferencedResourceTogglerWrapper.java
+++ src/com/futonredemption/jasper/togglers/PreferencedResourceTogglerWrapper.java
@@ -1,9 +1,33 @@
 package com.futonredemption.jasper.togglers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.futonredemption.jasper.SetOnceVariable;
+
+/** Manages the toggling a wrapped resourced based on implied ownership.
+ * Auto-toggling occurs if the app believes that it has taken control ownership by flag setting as to not
+ * conflict with the user's perceived.
+ * 
+ * IE: If the app wants to turn off wifi but the user manually turned it on, the app won't turn it off.
+*/
 public class PreferencedResourceTogglerWrapper extends AbstractResourceTogglerWrapper {
 
-	public PreferencedResourceTogglerWrapper(final IResourceToggler toggler) {
+	private final SetOnceVariable<SharedPreferences> prefs = new SetOnceVariable<SharedPreferences>() {
+		@Override
+		public SharedPreferences onSetVariable() {
+			return PreferenceManager.getDefaultSharedPreferences(context);
+		}
+	};
+	
+	private final Context context;
+	private final String statePreferenceName;
+	
+	public PreferencedResourceTogglerWrapper(final Context context, final IResourceToggler toggler, String statePreferenceName) {
 		super(toggler);
+		this.context = context;
+		this.statePreferenceName = statePreferenceName;
 	}
 
 	public boolean isSupported() {
@@ -19,10 +43,35 @@ public class PreferencedResourceTogglerWrapper extends AbstractResourceTogglerWr
 	}
 
 	public boolean enable() {
-		return toggler.enable();
+		
+		// Only enables if the resource is currently off and the mode save preference is disabled as well.
+		boolean performedEnable = false;
+		if(isDisabled()) {
+			performedEnable = toggler.enable();
+			setPreferenceValue(performedEnable);
+		}
+		
+		return performedEnable;
 	}
 
 	public boolean disable() {
-		return toggler.disable();
+		boolean performedDisable = false;
+		
+		if(isEnabled()) {
+			if(getPreferenceValue()) {
+				performedDisable = disable();
+				setPreferenceValue(! performedDisable);
+			}
+		}
+		
+		return performedDisable;
+	}
+	
+	protected boolean getPreferenceValue() {
+		return prefs.getValue().getBoolean(statePreferenceName, false);
+	}
+	
+	protected void setPreferenceValue(boolean newValue) {
+		prefs.getValue().edit().putBoolean(statePreferenceName, newValue).commit();
 	}
 }
